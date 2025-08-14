ce2939f7eb95e1bea8293e5d312951272a8ca5b2
ares <giovaniarias06@gmail.com>
2025-08-13 23:15:47 -0500

    feat: endpoint login con dto request validations

--- src/main/java/com/backend/estudiantes/dto/LoginRequest.java
+++ src/main/java/com/backend/estudiantes/dto/LoginRequest.java
@@ -1,7 +1,32 @@
 package com.backend.estudiantes.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
 
+    @NotBlank(message = "El Email es obligatorio")
+    @Email(message = "El Email debe ser valido")
     private String email;
+
+    @NotBlank(message = "La contraseia es obligatoria")
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
