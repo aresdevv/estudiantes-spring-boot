d213310e7dbb744408287140aecf66b36d19d622
ares <giovaniarias06@gmail.com>
2025-08-13 23:15:47 -0500

    feat: endpoint login con dto request validations

--- src/main/java/com/backend/estudiantes/controller/AuthController.java
+++ src/main/java/com/backend/estudiantes/controller/AuthController.java
@@ -1,9 +1,38 @@
 package com.backend.estudiantes.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backend.estudiantes.dto.LoginRequest;
+import com.backend.estudiantes.model.Usuario;
+import com.backend.estudiantes.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest){
+
+        try{
+            Usuario usuario = authService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+            return ResponseEntity.ok(Map.of(
+                    "message", "Login exitoso",
+                    "email", usuario.getEmail(),
+                    "rol", usuario.getRol()
+            ));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(Map.of(
+                    "error", e.getMessage()
+            ));
+        }
+
+
+    }
 }
