b2945bfcfbcd7ffbf7c26e9b0b672ead283a7d67
ares <giovaniarias06@gmail.com>
2025-09-08 18:24:31 -0500

    feat: agregó filtros de seguridad para autenticacion

--- src/main/java/com/backend/estudiantes/config/SecurityConfig.java
+++ src/main/java/com/backend/estudiantes/config/SecurityConfig.java
@@ -1,15 +1,30 @@
 package com.backend.estudiantes.config;
 
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth->
+                        auth.requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
