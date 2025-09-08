8f61181ab5b190d620c2110563091642a56845eb
ares <giovaniarias06@gmail.com>
2025-09-08 01:45:31 -0500

    feat: security config

--- src/main/java/com/backend/estudiantes/config/SecurityConfig.java
+++ src/main/java/com/backend/estudiantes/config/SecurityConfig.java
@@ -2,14 +2,17 @@ package com.backend.estudiantes.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableMethodSecurity
+
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+        public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
