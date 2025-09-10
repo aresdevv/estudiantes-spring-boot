d2748017a19dfbe9f20a5fc29c828adc7ab52203
ares <giovaniarias06@gmail.com>
2025-09-10 03:48:47 -0500

    feat: entidad RefeshToken

--- src/main/java/com/backend/estudiantes/model/RefreshToken.java
+++ src/main/java/com/backend/estudiantes/model/RefreshToken.java
@@ -1,4 +1,43 @@
 package com.backend.estudiantes.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "refreshtoken")
+@Getter
+@Setter
+@AllArgsConstructor
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn (name = "usuario_id", referencedColumnName = "id")
+    private Usuario usuario;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    public RefreshToken(Usuario usuario, String token, Instant expiryDate) {
+        this.usuario = usuario;
+        this.token = token;
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isExpired() {
+        return Instant.now().isAfter(expiryDate);
+    }
+
+    public RefreshToken() {}
 }
